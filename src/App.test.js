05785017062b3194da683d0c732e6161a2d35238
Kevser <kevser.sirca@binalyze.com>
2022-11-13 12:56:19 +0300

    refactor(qrcode): added qrcode component test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders QR Generator header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/QR Generator/i);
+  expect(headerElement).toBeInTheDocument();
 });
